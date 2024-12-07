c58951b3c46ad24e45c56114127ba54a60a9c10e
TEm <tem2k2.dev@gmail.com>
2024-12-07 13:05:22 +0700

    Use paramater default

--- app/src/main/java/com/example/myapplication/Extensions.kt
+++ app/src/main/java/com/example/myapplication/Extensions.kt
@@ -3,6 +3,6 @@ package com.example.myapplication
 import android.content.Context
 import android.widget.Toast
 
-fun Context.showToast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
 }
